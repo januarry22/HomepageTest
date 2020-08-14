05d71457bf89620e2d8a73558a9913abd9e68f8b
januarry22 <tls2330@naver.com>
2020-08-15 01:20:50 +0900

    modify01-2

--- src/com/test/dao/memberDAO.java
+++ src/com/test/dao/memberDAO.java
@@ -5,7 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.test.controller.DBconnect;
+import javax.naming.NamingException;
+
 import com.test.dbconn.DBConnection;
 import com.test.dto.joinDTO;
 
@@ -22,7 +23,7 @@ public class memberDAO {
 		return instance;
 	}
 
-	public void joinMember(joinDTO bean) {
+	public void joinMember(joinDTO bean) throws SQLException {
 
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -50,13 +51,60 @@ public class memberDAO {
 
 			conn.commit();
 
-		} catch (Exception sqle) {
-			try {
+		} catch (ClassNotFoundException | NamingException | SQLException sqle) {
 				conn.rollback();
-			} catch (SQLException e) {
-				// TODO: handle exception
-				e.printStackTrace();
+	            throw new RuntimeException(sqle.getMessage());
+		}finally{
+		try {
+			if (pstmt != null) {
+				pstmt.close();
 			}
+			if (conn != null) {
+				conn.close();
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+}
+
+	public int loginCheck(String id, String passwd1) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		String dbPW = ""; // db에서 꺼낸 비밀번호를 담을 변수
+		int x = -1;
+
+		try {
+			// 쿼리 - 먼저 입력된 아이디로 DB에서 비밀번호를 조회한다.
+			StringBuffer query = new StringBuffer();
+			query.append("SELECT passwd1 FROM shop WHERE ID=?");
+
+			conn = DBConnection.getConnection();
+			pstmt = conn.prepareStatement(query.toString());
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) // 입려된 아이디에 해당하는 비번 있을경우
+			{
+				dbPW = rs.getString("password1"); // 비번을 변수에 넣는다.
+
+				if (dbPW.equals(passwd1))
+					x = 1; // 비밀번호 db와 비교해서 확인
+				else
+					x = 0; // DB의 비밀번호와 입력받은 비밀번호 다름, 인증실패
+
+			} else {
+				x = -1; // 해당 아이디가 없을 경우
+			}
+
+			return x;
+
+		} catch (Exception sqle) {
+			throw new RuntimeException(sqle.getMessage());
 		} finally {
 			try {
 				if (pstmt != null) {
@@ -65,68 +113,14 @@ public class memberDAO {
 				if (conn != null) {
 					conn.close();
 				}
-
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-
-		}
-	}
-	
-	public int loginCheck(String id, String passwd1) 
-    {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
- 
-        String dbPW = ""; // db에서 꺼낸 비밀번호를 담을 변수
-        int x = -1;
- 
-        try {
-            // 쿼리 - 먼저 입력된 아이디로 DB에서 비밀번호를 조회한다.
-            StringBuffer query = new StringBuffer();
-            query.append("SELECT passwd1 FROM shop WHERE ID=?");
- 
-            conn = DBConnection.getConnection();
-            pstmt = conn.prepareStatement(query.toString());
-            pstmt.setString(1, id);
-            rs = pstmt.executeQuery();
- 
-            if (rs.next()) // 입려된 아이디에 해당하는 비번 있을경우
-            {
-                dbPW = rs.getString("password1"); // 비번을 변수에 넣는다.
- 
-                if (dbPW.equals(passwd1)) 
-                    x = 1; // 비밀번호 db와 비교해서 확인
-                else                  
-                    x = 0; // DB의 비밀번호와 입력받은 비밀번호 다름, 인증실패
-                
-            } else {
-                x = -1; // 해당 아이디가 없을 경우
-            }
- 
-            return x;
- 
-        } catch (Exception sqle) {
-            throw new RuntimeException(sqle.getMessage());
-        } finally {
-            try{
-        		if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
 				if (rs != null) {
 					rs.close();
 				}
-            }catch(Exception e){
-                throw new RuntimeException(e.getMessage());
-            }
-        }
-    } // end loginCheck()
-
-
+			} catch (Exception e) {
+				throw new RuntimeException(e.getMessage());
+			}
+		}
+	} // end loginCheck()
 
 	public void memberUpdate(joinDTO bean) {
 
@@ -243,12 +237,12 @@ public class memberDAO {
 
 	@SuppressWarnings("resource")
 	public int memberDelete(String id, String passwd1) {
-		
+
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
-		String dbPass="";
+
+		String dbPass = "";
 		int x = -1;
 
 		try {
@@ -262,29 +256,27 @@ public class memberDAO {
 			sql2.append("DELETE FROM shop WHERE id=?");
 
 			conn.setAutoCommit(false);
-			
-			pstmt=conn.prepareStatement(sql1.toString());
+
+			pstmt = conn.prepareStatement(sql1.toString());
 			pstmt.setString(1, id);
-			rs=pstmt.executeQuery();
-			
-			if(rs.next()) {
-			
-				dbPass=rs.getString("passwd1");
-				if(dbPass.equals(passwd1)) {
-					pstmt=conn.prepareStatement(sql2.toString());
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+
+				dbPass = rs.getString("passwd1");
+				if (dbPass.equals(passwd1)) {
+					pstmt = conn.prepareStatement(sql2.toString());
 					pstmt.setString(1, id);
 					pstmt.executeQuery();
 					conn.commit();
-					x=1; //삭제성공
-				}else {
-					x=0; //비밀번호 틀림
+					x = 1; // 삭제성공
+				} else {
+					x = 0; // 비밀번호 틀림
 				}
-					
 
 			}
 
-
-		} catch (Exception sqle){
+		} catch (Exception sqle) {
 			try {
 				conn.rollback();
 			} catch (SQLException e) {
